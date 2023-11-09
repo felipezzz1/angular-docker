5394b38ace384bfcbd6b6f40bcd434f7705c00f9
Felipe Zorzo <57020140034@unicentro.edu.br>
2023-11-09 14:46:37 +0000

    canLoad protection

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,43 +7,21 @@ import { ReviewsComponent } from "./restaurant-detail/reviews/reviews.component"
 import { OrderSummaryComponent } from "./order-summary/order-summary.component";
 import { NotFoundComponent } from "./not-found/not-found.component";
 import { LoginComponent } from "./security/login/login.component";
+import { LoggedInGuard } from "./security/loggedIn.guard";
 
 export const ROUTES:Routes = [
-    {
-        path: '',
-        component : HomeComponent
-    },
-    {
-        path: 'login',
-        component : LoginComponent
-    },
-    {
-        path: 'about',
-        loadChildren : './about/about.module#AboutModule'
-    },
-    {
-        path: 'restaurants',
-        component: RestaurantsComponent
-    },
-    {
-        path: 'restaurants/:id',
-        component: RestaurantDetailComponent,
+    {path: '', component : HomeComponent},
+    {path: 'login', component : LoginComponent},
+    {path: 'login/:to', component : LoginComponent},
+    {path: 'about', loadChildren : './about/about.module#AboutModule'},
+    {path: 'restaurants/:id', component: RestaurantDetailComponent,
         children:[
             {path : '', redirectTo : 'menu', pathMatch: 'full'},
             {path : 'menu', component: MenuComponent},
             {path : 'reviews', component: ReviewsComponent},
-        ]
-    },
-    {
-        path: 'order',
-        loadChildren: './order/order.module#OrderModule'
-    },
-    {
-        path: 'order-summary',
-        component : OrderSummaryComponent
-    },
-    {
-        path: '**',
-        component: NotFoundComponent
-    }
+        ]},
+    {path: 'restaurants', component: RestaurantsComponent},
+    {path: 'order', loadChildren: './order/order.module#OrderModule', canLoad:[LoggedInGuard]},
+    {path: 'order-summary', component : OrderSummaryComponent},
+    {path: '**', component: NotFoundComponent},
 ];
